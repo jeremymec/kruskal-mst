828c341c3cf61458f3d1a585a59dc265c6d0fda1
Jeremy Mechen <jeremymechen1997@gmail.com>
2017-06-27 23:15:14 +1200

    Working MST

--- src/DisjointSet.java
+++ src/DisjointSet.java
@@ -15,14 +15,21 @@ public class DisjointSet {
     }
 
     public Node find(Node n){
-        if (n.parent == n){
+        if (n.parent.equals(n)){
             return n;
         }
         return find(n.parent);
     }
 
     public void union(Node n1, Node n2){
-        n1.parent = n2;
+        Node n1Root = find(n1);
+        Node n2Root = find(n2);
+
+        if (n1Root.equals(n2Root)){
+            return;
+        }
+
+        n1Root.parent = n2Root;
         nodes.remove(n1);
     }
 
