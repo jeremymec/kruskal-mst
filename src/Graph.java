8802dd11dc136d3f7f9b1f61dbf1489e031afc5c
Jeremy Mechen <jeremymechen1997@gmail.com>
2017-06-27 19:20:37 +1200

    Fleshed out Graph Class; added fields

--- src/Graph.java
+++ src/Graph.java
@@ -1,17 +1,37 @@
+import java.util.ArrayList;
 
 public class Graph {
 
+    ArrayList<Node> nodes;
+    ArrayList<Edge> edges;
 
+    public Graph(){
+        nodes = new ArrayList<>();
+        edges = new ArrayList<>();
+    }
 
 }
 
 class Edge {
 
     int weight;
-    
+    Node n1;
+    Node n2;
+
+    public Edge(int weight, Node n1, Node n2){
+        this.weight = weight;
+        this.n1 = n1;
+        this.n2 = n2;
+    }
 
 }
 
 class Node {
+
+    char value;
+
+    public Node(char value){
+        this.value = value;
+    }
 
 }
